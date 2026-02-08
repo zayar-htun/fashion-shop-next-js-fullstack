388667122bfc1dc00f4c5e3d4f4f64f36b3ab2f0
zayyartun <zayyartun2842@gmail.com>
2026-02-08 17:35:24 +0400

    better auth configuration

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,12 @@
 import { emailOTPClient } from "better-auth/client/plugins";
+import { nextCookies } from "better-auth/next-js";
 import { createAuthClient } from "better-auth/react";
 
 export const { signIn, signUp, useSession, emailOtp } = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
-  plugins: [emailOTPClient()],
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  fetchOptions: {
+    credentials: "include",
+  },
+  plugins: [emailOTPClient(), nextCookies()],
 });
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,8 +1,11 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "@/lib/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000", // for seperate frontend/backend domains
+  //   trustedOrigins: ["http://localhost:3000"], // for seperate frontend/backend domains
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
@@ -13,6 +16,8 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      prompt: "select_account", // optional, forces account selection on every login
     },
   },
+  plugins: [nextCookies()],
 });
