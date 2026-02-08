0466e237e27f9d512075f8d5c5ff2c53b4177bcb
zayyartun <zayyartun2842@gmail.com>
2026-02-08 17:14:47 +0400

    email otp client plugins

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,8 @@
-import { createAuthClient } from "better-auth/react"
-export const  { signIn, signUp, useSession } = createAuthClient({
-    /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: "http://localhost:3000"
-})
+import { emailOTPClient } from "better-auth/client/plugins";
+import { createAuthClient } from "better-auth/react";
+
+export const { signIn, signUp, useSession, emailOtp } = createAuthClient({
+  /** The base URL of the server (optional if you're using the same domain) */
+  baseURL: "http://localhost:3000",
+  plugins: [emailOTPClient()],
+});
