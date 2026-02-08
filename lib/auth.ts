17be0a82bcc982f3fe185e3435f0802da38ca684
zayyartun <zayyartun2842@gmail.com>
2026-02-08 17:11:44 +0400

    better auth instance setup

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,12 +1,18 @@
-import { betterAuth } from 'better-auth'
-import { prismaAdapter } from 'better-auth/adapters/prisma'
-import {prisma} from '@/lib/prisma'
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@/lib/prisma";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: 'postgresql',
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
   },
-})
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
